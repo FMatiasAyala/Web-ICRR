8d2bd1ffd38ac6dec1a59cc80a2a37036c6590bd
FMatiasAyala <fabiomatiasayala@gmal.com>
2026-05-26 10:25:00 -0300

    fix: delete strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,11 @@ import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <HelmetProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </HelmetProvider>
-  </StrictMode>,
 )
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
